79edf9c90ac37900e971ffc5bb9f0bda96db3550
Pavel Trostianko <rengetsu@mail.ru>
2020-12-18 18:39:44 +0200

    Add files via upload

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        /**
+         * Non-production services
+         */
+        if ($this->app->environment() !== 'production') {
+            $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+            $this->app->register(\Laravel\Dusk\DuskServiceProvider::class);
+        }
+
+        /**
+         * Production-only services
+         */
+        if ($this->app->environment() === 'production') {
+            $this->app->register(\GrahamCampbell\HTMLMin\HTMLMinServiceProvider::class);
+        }
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
